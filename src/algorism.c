b1451fb198668d7e62402ed45eb6179ed978b29e
KiRist <37296174+KiRist-code@users.noreply.github.com>
2023-05-30 23:31:49 +0900

    Update algorism.c

--- src/algorism.c
+++ src/algorism.c
@@ -1,9 +1,9 @@
 #include "algorism.h"
 
-int isSame(int* list){
-    for(int i=0;i<6;i++){ //check some input number is same
-        for(int j=i+1;j<6;j++){
-            if(list[i] == list[j]) { 
+int isSame(int* list) {
+    for (int i = 0; i < 6; i++) { //check some input number is same
+        for (int j = i + 1; j < 6; j++) {
+            if (list[i] == list[j]) {
                 return 1;
             }
         }
@@ -11,28 +11,30 @@ int isSame(int* list){
     return 0;
 }
 
-int isExtraSame(int* list, int extra){
-    for(int i=0;i<6;i++){ //check some input number is same
-        if(list[i] == extra) { 
+int isExtraSame(int* list, int extra) {
+    for (int i = 0; i < 6; i++) { //check some input number is same
+        if (list[i] == extra) {
             return 1;
         }
     }
     return 0;
 }
 
-void swap(int* a, int* b){
-    int temp = *b;
+void swap(int* a, int* b) {
+    int* temp = *b;
     *b = *a;
     *a = temp;
 }
 
-void bubble_sort(int len, int* arr){
-    for(int i=0;i<len;i++){
-        for(int j=0;j<len;j++){
-            if(arr[j] > arr[j]){
-                swap(arr[j], arr[j+1]);
+int* bubble_sort(int len, int* arr) {
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j + 1]);
+                // 교수가 착하다 -> 이런 과제를 내준 교수가 만악의 근원
             }
         }
     }
-}
 
+    return arr;
+}
