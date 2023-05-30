be004cf864073f5fce224bf3b7b67becb8953de1
KiRist <37296174+KiRist-code@users.noreply.github.com>
2023-05-30 23:32:11 +0900

    Update algorism.h

--- src/includes/algorism.h
+++ src/includes/algorism.h
@@ -1,7 +1,13 @@
 #pragma once
+#if _WIN32 || _WIN64
+#include<windows.h>
+#define sleep(time) Sleep((time*1000))
+#else
+#include<unistd.h>
+#endif
 
 int isSame(int* list); //check "is Same?"
 int isExtraSame(int* list, int extra); //check extra number "is Same?"
 
 void swap(int* a, int* b);
-void bubble_sort(int len, int* arr);
+int* bubble_sort(int len, int* arr);
