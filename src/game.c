2128f3e3f56793afe43d7a887b6fc70b9af3792a
KiRist <37296174+KiRist-code@users.noreply.github.com>
2023-05-30 23:32:28 +0900

    Update game.c

--- src/game.c
+++ src/game.c
@@ -2,27 +2,28 @@
 #include"algorism.h"
 #include<stdlib.h>
 
-struct Lotto_struct *newGame(){
+struct Lotto_struct* newGame(struct Lotto_struct* lotto) {
     srand((unsigned int)time(NULL)); //set Seed based on time
 
-    struct Lotto_struct* lotto = (Lotto_struct*) calloc(1, sizeof(Lotto_struct)); //initialized new Lotto_struct
+    lotto = (Lotto_struct*)calloc(1, sizeof(Lotto_struct)); //initialized new Lotto_struct
 
-    for(int i=0;i<6;i++){
+    for (int i = 0; i < 6; i++) {
         lotto->number[i] = rand() % 45 + 1; //set random number
     }
     lotto->extra_num = rand() % 45 + 1; //set extra number
 
-    if(isSame(lotto->number)){
-        lotto = newGame(); //re-generate Lotto Game
+    if (isSame(lotto->number)) {
+        sleep(1);
+        lotto = newGame(lotto); //re-generate Lotto Game
     }
 
     return lotto;
 }
 
-int *compareNumber(int* numbers, int extra, Lotto_struct* lotto){
+int* compareNumber(int* numbers, int extra, Lotto_struct* lotto) {
     int count = 0;
-    for(int i=0;i<6;i++){
-        if(numbers[i] == lotto->number[i]){ //check user input is same with lotto number
+    for (int i = 0; i < 6; i++) {
+        if (numbers[i] == lotto->number[i]) { //check user input is same with lotto number
             count++;
         }
     }
@@ -31,10 +32,10 @@ int *compareNumber(int* numbers, int extra, Lotto_struct* lotto){
     case 6: // 6 numbers correct
         return 1;
     case 5: // 5 numbers correct
-        if(extra == lotto->extra_num){ //correct extra_number?
+        if (extra == lotto->extra_num) { //correct extra_number?
             return 2;
         }
-        else{
+        else {
             return 3;
         }
     case 4: // 4 numbers correct
@@ -46,8 +47,8 @@ int *compareNumber(int* numbers, int extra, Lotto_struct* lotto){
     }
 }
 
-void freeAllGame(Lotto_struct** lotto_ptr, int index){
-    for(int i=0;i<index;i++){
+void freeAllGame(Lotto_struct** lotto_ptr, int index) {
+    for (int i = 0; i < index; i++) {
         free(lotto_ptr[i]);
     }
     free(lotto_ptr);
